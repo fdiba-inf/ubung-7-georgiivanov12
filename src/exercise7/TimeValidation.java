a616ddbc54a0d6ca4c7c5232cca31ac0c94181ac
georgiivanov12 <Kiromorskia@abv.bg>
2020-11-14 20:57:01 +0000

    Ubung 7

--- src/exercise7/TimeValidation.java
+++ src/exercise7/TimeValidation.java
@@ -1,7 +1,5 @@
 package exercise7;
-
 import java.util.Scanner;
-
 public class TimeValidation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -12,19 +10,29 @@ public class TimeValidation {
         System.out.print("Enter minutes: ");
         int minutes = input.nextInt();
 
-        boolean timeValid = false; // Call validation methods
+        boolean timeValid = false; 
+        if (areHoursValid(hours) && areMinutesValid(minutes))
+        {
+            timeValid=true;
+        }
         System.out.println("Time valid: " + timeValid);
     }
 
 
     public static boolean areHoursValid(int hours) {
-        // Implement method
+        if(hours>=0 && hours<24)
+        {
+            return true;
+        }
         return false;
     }
 
 
     public static boolean areMinutesValid(int minutes) {
-        // Implement method
+        if (minutes>=0 && minutes<60)
+        {
+            return true;
+        }
         return false;
     }
 }
